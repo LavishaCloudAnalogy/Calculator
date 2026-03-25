ba69c4f427fcd585bab983c14386c50f30f38860
Lavisha <lavisha@cloudanalogy.com>
2026-03-25 12:59:00 +0530

    final changes

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const calculator_js_1 = require("./utils/calculator.js");
-const expression = "3 + 4 * 2 / ( 1 - 5 )";
-const res = (0, calculator_js_1.calculate)(expression);
-console.log(res);
